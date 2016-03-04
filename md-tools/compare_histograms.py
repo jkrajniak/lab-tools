2915315b54cbff614a7046e297213e313285858a
Jakub Krajniak <jkrajniak@gmail.com>
2016-03-04 11:42:45 +0100

    added chi-square shape test

--- md-tools/compare_histograms.py
+++ md-tools/compare_histograms.py
@@ -19,6 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import numpy as np
+from scipy.stats import chisqprob
 
 
 def _args():
@@ -27,6 +28,8 @@ def _args():
     parser.add_argument('data_2')
     parser.add_argument('--min_max', help='Histogram range, format min:max')
     parser.add_argument('--bins', type=int, help='Number of bins')
+    parser.add_argument('--test_type', help='Name of test')
+    parser.add_argument('--frames', type=int, default=None)
 
     return parser.parse_args()
 
@@ -38,20 +41,57 @@ def test_squared_diff(hist_1, hist_2):
     return np.sum(np.power(diff, 2))
 
 
+def chi_square(hist_1, hist_2):
+    diff_1 = hist_1 - hist_2
+    val = np.sum((np.power(diff_1, 2))/(hist_1+hist_2))
+    ddof = len(hist_1)
+
+    print('T = {}'.format(val))
+    print('P(chi^2 > T) = {}'.format(chisqprob(val, ddof)))
+
+
+def chi_square_shape(hist_1, hist_2):
+    n1 = np.sum(hist_1)
+    n2 = np.sum(hist_2)
+    diff_1 = (hist_1/n1) - (hist_2/n2)
+    sum_1 = (hist_1/(n1*n1)) + (hist_2/(n2*n2))
+    val = np.sum((np.power(diff_1, 2))/sum_1)
+    ddof = len(hist_1) - 1
+
+    print ('T = {}'.format(val))
+    print('P(chi^2 > T) = {}'.format(chisqprob(val, ddof)))
+
+
 def main():
     args = _args()
 
+    tests = {
+        'squared_diff': test_squared_diff,
+        'chi_square': chi_square,
+        'chi_square_shape': chi_square_shape
+    }
+    if args.test_type not in tests:
+        print('--test_type not found, available: {}'.format(tests.keys()))
+        return
+    print('Reading {}'.format(args.data_1))
     data_1 = np.loadtxt(args.data_1)
+    print('Reading {}'.format(args.data_2))
     data_2 = np.loadtxt(args.data_2)
+    print('Data read')
+
+    if args.frames:
+        data_1 = data_1[:args.frames]
+        data_2 = data_2[:args.frames]
 
     min_bins, max_bins = map(float, args.min_max.split(':'))
     bins = np.arange(min_bins, max_bins, (max_bins-min_bins)/args.bins)
 
     # Create histograms with the same bins.
-    histogram_1, _ = np.histogram(data_1, bins=bins)
-    histogram_2, _ = np.histogram(data_2, bins=bins)
+    histogram_1, _ = np.histogram(data_1, bins=bins, density=False)
+    histogram_2, _ = np.histogram(data_2, bins=bins, density=False)
 
-    print('f={}'.format(test_squared_diff(histogram_1, histogram_2)))
+    print('Running test {}'.format(args.test_type))
+    tests[args.test_type](histogram_1, histogram_2)
 
 
 if __name__ == '__main__':
