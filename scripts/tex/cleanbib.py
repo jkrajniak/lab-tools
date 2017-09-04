5b7e7463b64fae022ad2fcf97c8e74fa737f3908
Jakub Krajniak <jkrajniak@gmail.com>
2017-09-04 11:14:44 +0200

    updated cleanbib

--- scripts/tex/cleanbib.py
+++ scripts/tex/cleanbib.py
@@ -1,26 +1,73 @@
 # Script to clean (and sort) the bibfile exported by mendeley
 
 # You will need bibtexparser package:
-# $ pip install bibtexparser
+# $ pip3 install bibtexparser
+# and Python 3.x
 
-# usage: cleanbib.py library.bib
-# WARNING: it overwrites the original file
+# usage: cleanbib.py library.bib output.bib
+#
 
+import argparse
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
+from bibtexparser.bwriter import BibTexWriter
+from bibtexparser import customization
+import re
 import sys
 
-if __name__ == '__main__':
-    bibfile = sys.argv[1]
+def _args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_bib', help='Input .bib file')
+    parser.add_argument('output_bib', help='Output .bib file')
+
+    parser.add_argument('--clean', action='store_true')
+    parser.add_argument('--title', action='store_true')
+
+    return parser.parse_args()
+
+def protect_uppercase(string):
+    """
+    Protect uppercase letters for bibtex
+
+    :param string: string to convert
+    :returns: string
+    """
+    string = re.sub('([^{]|^)([A-Z])([^}]|$)', '\g<1>{\g<2>}\g<3>', string)
+    return string
+
+def keep_uppercase(record):
+    # And then, we fall back
+    for val in record:
+        if val not in ('ID',):
+            if val in ['title', 'author', 'journal']:
+                record[val] = protect_uppercase(record[val])
+    return record
+
+
+def main():
+    args = _args()
+
+    bibfile = args.input_bib
     with open(bibfile) as bibtex_file:
         parser = BibTexParser()
+        parser.ignore_nonstandard_types = False
+        parser.homogenize_fields = True
+        parser.common_strings = True
+        parser.customization = keep_uppercase
         bib_database = bibtexparser.load(bibtex_file, parser=parser)
 
+        if args.clean:
+            for entry in bib_database.entries:
+                for k in ('file', 'annote', 'abstract', 'url', 'file', 'link'):
+                    entry.pop(k,None)
+
         for entry in bib_database.entries:
-            for k in ('file', 'annote', 'abstract', 'url', 'file', 'link'):
-                entry.pop(k,None)
+            entry['title'] = '{{{}}}'.format(entry['title'])
 
-        bibtex_string = bibtexparser.dumps(bib_database)
+        bibwriter = BibTexWriter()
+        with open(args.output_bib, 'w') as outbib:
+            bibtexparser.dump(bib_database, outbib, bibwriter)
 
-        with open(bibfile,'w') as bibtex_file:
-            bibtex_file.write(bibtex_string.encode('utf8'))
+
+if __name__ == '__main__':
+    main()
